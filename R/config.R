c4ac8a23116fec0cc4214f28ad3768749562fce1
Joli Holmes <jh111@ad.rice.edu>
2019-04-16 15:05:28 -0500

    updated the read_json function

--- R/config.R
+++ R/config.R
@@ -1,30 +1,30 @@
-read_yaml <- function(pth){
-  #' Reads a yaml file
-  #'
-  #' Reads a yaml file
-  #' @param pth the path to the yaml file
-  #' @return a nested list
-  #' @examples
-  #' config <- read_yaml("C:/Documents/config.yaml")
-
-  config <- yaml::yaml.load_file(file.path(pth))
-
-  return(config)
-
+read_yaml <- function(pth) {
+    #' Reads a yaml file
+    #'
+    #' Reads a yaml file
+    #' @param pth the path to the yaml file
+    #' @return a nested list
+    #' @examples
+    #' config <- read_yaml('C:/Documents/config.yaml')
+    
+    config <- yaml::yaml.load_file(file.path(pth))
+    
+    return(config)
+    
 }
 
-read_json <- function(pth){
-  #' Reads a json file
-  #'
-  #' Reads a json file
-  #' @param pth the path to the yaml file
-  #' @return a nested list
-  #' @examples
-  #' config <- read_json("C:/Documents/config.json")
-  
-  
-  config <- rjson::fromJSON(file = file.path(pth))
-
-  return(config)
-
+read_json <- function(pth) {
+    #' Reads a json file
+    #'
+    #' Reads a json file
+    #' @param pth the path to the yaml file
+    #' @return a nested list
+    #' @examples
+    #' config <- read_json('C:/Documents/config.json')
+    
+    
+    config <- rjson::fromJSON(file = file.path(pth))
+    
+    return(config)
+    
 }
