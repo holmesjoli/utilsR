d012f3ce979f3d3b288fcc3809b4fb87eb0bf8ea
Joli Holmes <jh111@ad.rice.edu>
2019-04-16 16:37:14 -0500

    updated docstrings

--- R/logs.R
+++ R/logs.R
@@ -4,27 +4,28 @@ open_log <- function(fl) {
     #' Opens a log file
     #' @param fl the filename of the log
     #' @examples
-    #'
     #' fl <- 'test_log.log'
     #' open_log(fl)
-    
+
     op <- options(digits.secs = 0)
     start_tm <- Sys.time()
-    
+
     sink(fl)
     cat(paste0("Log file initiated on ", start_tm, "\n"))
     cat(hash)
 }
 
 close_log <- function() {
-    #' Closes a log file
-    
+    #' Closes the open log file
+    #' @examples'
+    #' close_log()
+
     end_tm <- Sys.time()
     duration <- end_tm - start_tm
-    
+
     cat(hash)
     cat(paste0("Log file closed on ", end_tm, "\n"))
     cat(paste0("Took ", round(duration, 1), " seconds to run."))
-    
+
     sink()
 }
