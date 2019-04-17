01b2353d9378e59d902afa2b67e7d240bd026599
Joli Holmes <jh111@ad.rice.edu>
2019-04-17 11:31:49 -0500

    added functions to export yaml and json files

--- R/config.R
+++ R/config.R
@@ -5,7 +5,8 @@ read_yaml <- function(pth) {
     #' @param pth the path to the yaml file
     #' @return a nested list
     #' @examples
-    #' config <- read_yaml('./tests/testthat/test.yaml')
+    #' pth <- "./tests/testthat/test.yaml"
+    #' config <- read_yaml(pth)
 
     config <- yaml::yaml.load_file(file.path(pth))
 
@@ -20,11 +21,43 @@ read_json <- function(pth) {
     #' @param pth the path to the yaml file
     #' @return a nested list
     #' @examples
-    #' config <- read_json('./tests/testthat/test.json')
-
+    #' pth <- "./tests/testthat/test.json"
+    #' config <- read_json(pth)
 
     config <- rjson::fromJSON(file = file.path(pth))
 
     return(config)
 
+}
+
+update_yaml <- function(config, pth){
+    #' Exports a yaml file
+    #'
+    #' Exports a yaml file
+    #' @param config the nested list to output as a configuration file
+    #' @param pth the path to the yaml file
+    #' @examples
+    #' pth <- "./tests/testthat/test.yaml"
+    #' config <- read_yaml(pth)
+    #' config$test3 <- 3
+    #' update_yaml(pth)
+
+    yml <- yaml::as.yaml(config)
+    write(yml, file.path(pth))
+}
+
+update_json <- function(config, pth) {
+    #' Updates a json file
+    #'
+    #' Updates a json file
+    #' @param config the nested list to output as a configuration file
+    #' @param pth the path to the yaml file
+    #' @examples
+    #' pth <- "./tests/testthat/test.json"
+    #' config <- read_json(pth)
+    #' config$test3 <- 3
+    #' update_json(config, pth)
+
+    json <- rjson::toJSON(config)
+    jsonlite::write_json(config, file.path(pth))
 }
