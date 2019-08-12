d7c3c2c228cc854d02b74023d67d7c391d460cfc
Joli Holmes <jh111@ad.rice.edu>
2019-08-12 11:03:39 -0500

    initial commit

---
+++ R/read.R
@@ -0,0 +1,12 @@
+#' @title Read excel all
+#' @description Reads in all excel sheets from an excel file
+#' @param pth the path to read the excel sheet from
+read_excel_all <- function(pth, ...) {
+
+  sheets <- readxl::excel_sheets(pth)
+  xl <- lapply(sheets, function(x) {readxl::read_excel(pth, sheet = x)})
+  names(xl) <- sheets
+
+  return(xl)
+
+}
